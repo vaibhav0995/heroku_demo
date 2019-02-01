df06436aaeee7a5920445f2de21572891b6cd166
unknown <vaibhav.gautam@metacube.com>
2019-02-01 15:28:36 +0530

    asdasd

--- src/main/java/com/metacube/training/EmployeePortalSpringBootApplication.java
+++ src/main/java/com/metacube/training/EmployeePortalSpringBootApplication.java
@@ -3,8 +3,6 @@ package com.metacube.training;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.example.Main;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
